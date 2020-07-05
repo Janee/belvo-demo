ec1a0f5ad60c7822d92d9d31c1ac3b815f3fcb88
Janeé Peña <janee.pena@gmail.com>
2020-07-05 15:31:41 -0500

    Add belvo API methods to project

---
+++ app/services/belvo_manager/owners.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: false
+
+require 'belvo'
+
+module  BelvoManager
+  # https://developers.belvo.co/docs/access-your-first-financial-data
+  class Owners
+    def initialize
+      @belvo = Belvo::Client.new(
+        Rails.application.credentials.belvo[:secret_id],
+        Rails.application.credentials.belvo[:secret_pass],
+        'https://sandbox.belvo.co'
+      )
+    end
+
+    # BelvoManager::Owners.new.listed
+    def listed
+      @belvo.owners.list
+    rescue Belvo::RequestError => e
+      puts e.status_code
+      puts e.detail
+    end
+
+    # BelvoManager::Owners.new.get_by_id(data)
+    # data example: { id: 9e926ee9-611a-4255-8554-bd730bc0bd2f }
+    def get_by_id(data)
+      @belvo.owners.detail(id: data)
+    rescue Belvo::RequestError => e
+      puts e.status_code
+      puts e.detail
+    end
+  end
+end
