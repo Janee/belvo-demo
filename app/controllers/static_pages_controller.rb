d528c56c4a5b86ecb498385a1a699aab507349eb
Janeé Peña <janee.pena@gmail.com>
2020-07-05 23:54:44 -0500

    Add balance chart grouped by currency

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: false
+
 class StaticPagesController < ApplicationController
+
+  # Dashboard
+  def index
+    @balances = BelvoManager::Balances.new
+    
+    @currencies = format(@balances.overview)
+  end
+
+  def format(value)
+    currencies = value.group_by { |k| k[:currency] }
+    currencies.each do |cu|
+      aux = []
+      cu[1].each do |ba|
+        aux << ba.except(:currency).values
+      end
+      cu[1] << { values: aux }
+    end
+    currencies
+  end
 end
--- app/services/belvo_manager/balances.rb
+++ app/services/belvo_manager/balances.rb
@@ -4,7 +4,7 @@ require 'belvo'
 
 module  BelvoManager
   # https://developers.belvo.co/docs/access-your-first-financial-data
-  class Balance
+  class Balances
     def initialize
       @belvo = Belvo::Client.new(
         Rails.application.credentials.belvo[:secret_id],
@@ -21,13 +21,26 @@ module  BelvoManager
       puts e.detail
     end
 
+    # BelvoManager::Balance.new.overview
+    def overview
+      @balances = []
+      @belvo.balances.list.each do |current|
+        balance = {}
+        balance[:category] = current['account']['category']
+        balance[:currency] = current['account']['currency']
+        balance[:amount] = current['current_balance']
+        @balances << balance
+      end
+      @balances
+    end
+
     # BelvoManager::Balance.new.global
     def global
       @total = {}
       @belvo.balances.list.each do |current|
         key = current['account']['currency']
-        @total[key] = 0 unless @total.key?(key)
-        @total[key] += current['current_balance']
+        @total[key.to_sym] = 0 unless @total.key?(key)
+        @total[key.to_sym] += current['current_balance']
       end
       @total
     rescue Belvo::RequestError => e
